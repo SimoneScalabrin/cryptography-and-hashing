a41e929ab490d9084a8687feaf0a5eaf1811bb67
Scalabrin Simone (CSS DSI D SW PLF CLV) <Simone.Scalabrin@infineon.com>
2026-04-13 09:18:10 +0200

    Add NaivePrime and NaivePrimeTest classes for primality testing in java

---
+++ java_implementation/Asym/PrimeNumbers/NaivePrime.java
@@ -0,0 +1,11 @@
+package PrimeNumbers;
+
+public class NaivePrime {
+    public static void main(String[] args) {
+    
+        NaivePrimeTest primeTest = new NaivePrimeTest();
+
+        System.out.println(primeTest.isPrime(17));
+
+    }
+}
